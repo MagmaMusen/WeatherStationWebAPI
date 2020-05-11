a17a573011a1ba577a883077d55357ab1ea0afe1
GitDal <jeppedybdal@gmail.com>
2020-05-11 17:37:45 +0200

    Unique

---
+++ WeatherStationWebAPI/Migrations/20200511153623_UniquePlaceProperties.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeatherStationWebAPI.Migrations
+{
+    public partial class UniquePlaceProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Places_Latitude_Longitude",
+                table: "Places",
+                columns: new[] { "Latitude", "Longitude" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Places_Latitude_Longitude",
+                table: "Places");
+        }
+    }
+}
